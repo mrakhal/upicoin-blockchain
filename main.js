d5b3df22eff8c212e39a33895f59e84beb4f21f8
mrakhal <mochamadrakha@gmail.com>
2021-08-25 19:46:54 +0700

    upicoin-blockchain-v0.1 : Proof of Work

--- main.js
+++ main.js
@@ -7,17 +7,27 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
 
+    //PROOF OF WORK
+    mineBlock(difficulty){
+        while(this.hash.substring(0,difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
     createGenesisBlock(){
         return new Block(0,"25/08/2020","Sentimental Block","0");
@@ -29,7 +39,7 @@ class Blockchain{
     
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -51,12 +61,9 @@ class Blockchain{
 }
 
 let upicoin = new Blockchain();
+
+console.log('Mining Block 1 ')
 upicoin.addBlock(new Block(1,"23/08/2020",{amount: 4 }));
+console.log('Mining Block 2 ')
 upicoin.addBlock(new Block(2,"25/08/2020",{amount: 10 }));
 
-
-
-console.log('Cek Blockchain Valid ? ' + upicoin.isChainValid());
-upicoin.chain[1].data = { amount: 100 };
-upicoin.chain[1].hash = upicoin.chain[1].calculateHash();
-// console.log(JSON.stringify(upicoin,null,4));
